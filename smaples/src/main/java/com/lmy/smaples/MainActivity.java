fb594aea93b7dc3ebe687ab0b6c73239d3a725ab
lrlmy <425078310@qq.com>
2016-03-28 18:23:43 +0800

    增加测试

--- smaples/src/main/java/com/lmy/smaples/MainActivity.java
+++ smaples/src/main/java/com/lmy/smaples/MainActivity.java
@@ -3,11 +3,16 @@ package com.lmy.smaples;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.lmy.lycommon.gles.widget.CameraGLSurfaceView;
+import com.lmy.lycommon.gles.widget.ShotGLSurfaceView;
+
 public class MainActivity extends AppCompatActivity {
 
+    private CameraGLSurfaceView view;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        view=(CameraGLSurfaceView)findViewById(R.id.shot_view);
     }
 }
